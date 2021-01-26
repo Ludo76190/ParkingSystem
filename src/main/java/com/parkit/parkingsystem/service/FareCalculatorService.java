fade8548788843818bba29beaf160b68f451d4a0
Ludovic Allegaert <allegaertl@gmail.com>
2021-01-26 10:14:28 +0100

    add 30Min free

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,14 +10,19 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime());
         }
 
-        //getTime renvoie des millisecondes
+        // getTime renvoie des millisecondes
         double inHour = ticket.getInTime().getTime();
         double outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        //diviser duration par 60.0*60*1000 pour convertir en heure
-        //il faut coder 60.0 dans la division pour avoir le type double
+        // diviser duration par 60.0*60*1000 pour convertir en heure
+        // il faut coder 60.0 dans la division pour avoir le type double
         double duration = (outHour - inHour)/(60.0*60*1000);
+
+        // Free 30 Minutes
+        if (duration <= 0.5) {
+            duration=0;
+        }
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
